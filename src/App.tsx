c9cd4b300da986bfd2e724dce0deb05088345714
Mark Guiang <mahikgot@gmail.com>
2024-05-16 19:04:27 +0800

    implement zooming and panning

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,44 @@ export default function App() {
         canvas.on('selection:cleared', () => {
             selectedImage.current = null;
         });
-
+        canvas.on('mouse:down', function(opt) {
+          var evt = opt.e;
+          if (evt.metaKey === true) {
+            this.isDragging = true;
+            this.selection = false;
+            this.lastPosX = evt.clientX;
+            this.lastPosY = evt.clientY;
+          }
+        });
+        canvas.on('mouse:move', function(opt) {
+          if (this.isDragging) {
+            var e = opt.e;
+            var vpt = this.viewportTransform;
+            vpt[4] += e.clientX - this.lastPosX;
+            vpt[5] += e.clientY - this.lastPosY;
+            this.requestRenderAll();
+            this.lastPosX = e.clientX;
+            this.lastPosY = e.clientY;
+          }
+        });
+        canvas.on('mouse:up', function(opt) {
+          // on mouse up we want to recalculate new interaction
+          // for all objects, so we call setViewportTransform
+          this.setViewportTransform(this.viewportTransform);
+          this.isDragging = false;
+          this.selection = true;
+        });
+        canvas.on('mouse:wheel', function(opt) {
+          var delta = opt.e.deltaY;
+          var zoom = canvas.getZoom();
+          zoom *= 0.999 ** delta;
+          if (zoom > 20) zoom = 20;
+          if (zoom < 0.01) zoom = 0.01;
+          canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+          opt.e.preventDefault();
+          opt.e.stopPropagation();
+        });
+        
         canvasRef.current = canvas;
         return () => {
             canvas.dispose();
