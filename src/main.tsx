35a45882f8bbf44265f4d06eccc0846712ed6421
Mark Guiang <mahikgot@gmail.com>
2024-05-24 18:36:34 +0800

    suppress ts errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// @ts-ignore
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
