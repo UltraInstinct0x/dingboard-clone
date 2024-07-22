ea4e715c9ed8862ade1908b0d008b5b866b275c7
Mark Guiang <mahikgot@gmail.com>
2024-07-23 04:55:24 +0800

    remove header vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,10 +17,4 @@ export default defineConfig({
           ]
         }),
     ],
-    server: {
-        headers: {
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Embedder-Policy': 'require-corp',
-        }
-    },
 })
