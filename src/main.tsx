014986103a4dd172ed59ff9cd7b15845bd5a8517
Mark Guiang <mahikgot@gmail.com>
2024-07-23 05:36:05 +0800

    remove comments

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'react-tooltip/dist/react-tooltip.css'
-// @ts-ignore
-//import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-//serviceWorkerRegistration.register();
